b139683fe30c8fea14f47a5af81e9c38e2a6dae3
axyz <axyz@Nileshs-MacBook-Air.local>
2022-06-08 13:59:49 +0530

    Lecture 23

---
+++ Lec23/RecursionDemo.java
@@ -0,0 +1,78 @@
+package Lec23;
+
+public class RecursionDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		PI2(5,10);
+//		PDI(5);
+		System.out.println(sum(6));
+
+	}
+	
+	public static void PD(int n)
+	{
+		if(n == 1)
+		{
+			System.out.println(1);
+		}
+		else
+		{
+			System.out.println(n);
+			PD(n-1);
+		}
+	}
+	public static void PI(int n)
+	{
+		if(n == 1)
+		{
+			System.out.println(1);
+		}
+		else
+		{
+			PI(n-1);
+			System.out.println(n);
+		}
+	}
+	
+	public static void PI2(int i,int n)
+	{
+		if(n == i)
+		{
+			System.out.println(n);
+		}
+		else
+		{
+			System.out.println(i);
+			PI2(i+1,n);
+		}
+	}
+	
+	
+	public static void PDI(int n)
+	{
+		if(n == 1)
+		{
+			System.out.println(1);
+		}
+		else
+		{
+			System.out.println(n);
+			PDI(n-1);
+			System.out.println(n);
+		}
+	}
+	
+	public static int sum(int n)
+	{
+		if(n == 1)
+		{
+			return 1;
+		}
+		else {
+			return sum(n-1)+n;
+		}
+	}
+	
+
+}
